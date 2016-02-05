51eaba8fdeaea899e7a6df10d55760bd6b88b075
dhani1803 <dhani.s.wibowo@gmail.com>
2016-02-05 17:04:04 +0700

    Adeed kelas tunjangan

---
+++ Sisikaryakan/Karyawan.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sisikaryakan
+{
+    class Karyawan
+    {
+        public String _nama;
+        public String _jabatan;
+        public DateTime _tanggalMasukKerja;
+        public String _assignment;
+        public String _pemberhentian;
+
+        public Karyawan()
+        {
+        }
+
+        public String nama
+        {
+            get { return _nama; }
+            set { _nama = nama; }
+        }
+
+        public String jabatan
+        {
+            get { return _jabatan; }
+            set { _jabatan = jabatan; }
+        }
+
+        public DateTime tanggalMasukKerja
+        {
+            get { return _tanggalMasukKerja; }
+            set { _tanggalMasukKerja = tanggalMasukKerja; }
+        }
+
+
+        public String assignment
+        {
+            get { return _assignment; }
+            set { _assignment = assignment; }
+        }
+
+        public String pemberhentian
+        {
+            get { return _pemberhentian; }
+            set { _pemberhentian = pemberhentian; }
+        }
+
+
+
+
+    }
+}
---
+++ Sisikaryakan/Tunjangan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sisikaryakan
+{
+    class Tunjangan
+    {
+        public String _training;
+        public int _gajiPokok;
+        public int _tunjanganKerja;
+        public int _tunjanganPenginapan;
+        public double _tunjanganIBanding;
+        public double _tunjanganKesehatan;
+
+
+
+
+    }
+}
