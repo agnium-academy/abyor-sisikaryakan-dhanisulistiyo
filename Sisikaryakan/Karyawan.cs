bfa52ef1e8cf6c319b2b9074c3001db3b62cf73a
dhani1803 <dhani.s.wibowo@gmail.com>
2016-02-09 14:12:20 +0700

    Update and Edit Class karyawan

--- Sisikaryakan/Karyawan.cs
+++ Sisikaryakan/Karyawan.cs
@@ -6,51 +6,13 @@ using System.Threading.Tasks;
 
 namespace Sisikaryakan
 {
-    class Karyawan
+    abstract class Karyawan
     {
-        public String _nama;
-        public String _jabatan;
-        public DateTime _tanggalMasukKerja;
-        public String _tugas;
-        public String _pemberhentian;
-
-        public Karyawan()
-        {
-        }
-
-        public String nama
-        {
-            get { return _nama; }
-            set { _nama = nama; }
-        }
-
-        public String jabatan
-        {
-            get { return _jabatan; }
-            set { _jabatan = jabatan; }
-        }
-
-        public DateTime tanggalMasukKerja
-        {
-            get { return _tanggalMasukKerja; }
-            set { _tanggalMasukKerja = tanggalMasukKerja; }
-        }
-
-
-        public String tugas
-        {
-            get { return _tugas; }
-            set { _tugas = tugas; }
-        }
-
-        public String pemberhentian
-        {
-            get { return _pemberhentian; }
-            set { _pemberhentian = pemberhentian; }
-        }
-
-
-
+        public string _nama;
+        public string _jabatan;
+        public string _tanggalMasukKerja;
+        public string _tugas;
+        public string _pemberhentian;
 
     }
 }
