b39b128b7fc6e5cb8beffc2667c11916faa11024
dhani1803 <dhani.s.wibowo@gmail.com>
2016-02-09 14:12:31 +0700

    Added class Manager

---
+++ Sisikaryakan/Manager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sisikaryakan
+{
+    class Manager : Supervisor
+    {
+        public int _tunjanganHariRaya;
+        public int _tunjanganStudyBanding;
+        public int _tunjanganKesehatan;
+
+        public Manager()
+        {
+
+        }
+
+        public Manager(string jabatan)
+        {
+            this._jabatan = jabatan;
+        }
+
+
+
+    }
+}
