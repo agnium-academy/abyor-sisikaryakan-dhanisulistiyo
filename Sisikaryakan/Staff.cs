e423ea5baae624b3ccb6d3b720363a1e49a6a38c
dhani1803 <dhani.s.wibowo@gmail.com>
2016-02-09 10:14:31 +0700

    added class staff

---
+++ Sisikaryakan/Staff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sisikaryakan
+{
+    class Staff : Karyawan
+    {
+        public String _gajiPokok;
+        public String _tunjangan;
+
+
+
+    }
+}
